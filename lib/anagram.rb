baac543ed620eee17e3eed98a1a6a5051d1bb99b
John Werenski <jtwerenski@gmail.com>
2018-09-22 15:04:33 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,10 +6,8 @@ class Anagram
     @word = word
   end 
   
-  def match(%w(string)) 
-    #binding.pry 
-    #string.delete_prefix('"').delete_suffix('"')
-    #binding.pry
+  def match(string)
+    
     string.select do |string_word|
       string_word.split("").sort == @word.split("").sort 
     end
